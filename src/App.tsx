38195ce128b634420da8731dc1de73a805ee3f99
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-08 16:15:20 -0300

    Simple usage of props system

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
+import Practice from './components/Practice';
 
 function App() {
+  
   return (
     <div className="form-container">
       <Login />
+      <Practice name="pedro"/>
     </div>
   );
 }
---
+++ src/components/Practice.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Practice(props: any) {
+
+    return(
+        <h1>Hello {props.name}</h1>
+    );
+}
+
+export default Practice;
