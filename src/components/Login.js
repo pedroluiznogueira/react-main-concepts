10cb4b7aa342ee87949dda8878e21354fa6d44ac
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-08 17:17:08 -0300

    When component is rendered, it's constructor will be executed, looking into this.state = true, so the button is shown, if the button is clicked, the state is going to be setted to false, and the expression will set the button to show 'OFF'

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,55 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
 
-function Login() {
+class Login extends React.Component {
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("It was clicked");
+  constructor(props) {
+    super(props);
+    this.state = { isToggledOn: true };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-    return (
-        <><Form className="form-content" onSubmit={handleSubmit}> 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+  handleSubmit(e) {
+    e.preventDefault();
+  }
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-    </Form></>
+  handleClick() {
+    this.setState(state => ({ 
+      isToggledOn: !state.isToggledOn
+    }))
+  }
+
+    render() {
+      return (
+        <><Form className="form-content" onSubmit={this.handleSubmit}> 
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={this.handleClick}>
+            {this.state.isToggledOn ? 'ON' : 'OFF'}
+          </Button>
+        </Form></>
     );
+    }
 }
+
+ReactDOM.render(
+  <Login />,
+  document.getElementById('root')
+);
 
 export default Login;
